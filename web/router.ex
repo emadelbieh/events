a10b5e5314646d050cbbd796370ff705c8056a1b
KL <kl.calamay@blackswan.ventures>
2017-04-13 09:48:15 +0800

    Generates uuid on /uuid (get ATM)

--- web/router.ex
+++ web/router.ex
@@ -17,6 +17,7 @@ defmodule Events.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    get "/uuid", IdentityController, :generate
   end
 
   # Other scopes may use custom stacks.
