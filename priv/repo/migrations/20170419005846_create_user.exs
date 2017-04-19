45d92ad91506b9a510bdd75625dc1e3e9abc5567
KL <kl.calamay@blackswan.ventures>
2017-04-19 09:45:31 +0800

    RDS support for identity

---
+++ priv/repo/migrations/20170419005846_create_user.exs
@@ -0,0 +1,13 @@
+defmodule Events.Repo.Migrations.CreateUser do
+  use Ecto.Migration
+
+  def change do
+    create table(:users, primary_key: false) do
+      add :id,      :uuid, primary_key: true
+      add :context, :map
+
+      timestamps()
+    end
+
+  end
+end
---
+++ test/models/user_test.exs
@@ -0,0 +1,18 @@
+defmodule Events.UserTest do
+  use Events.ModelCase
+
+  alias Events.User
+
+  @valid_attrs %{context: %{}, uuid: "7488a646-e31f-11e4-aace-600308960662"}
+  @invalid_attrs %{}
+
+  test "changeset with valid attributes" do
+    changeset = User.changeset(%User{}, @valid_attrs)
+    assert changeset.valid?
+  end
+
+  test "changeset with invalid attributes" do
+    changeset = User.changeset(%User{}, @invalid_attrs)
+    refute changeset.valid?
+  end
+end
