d6d3d18cacd7e5d003960b8cc80bde0e9f765fe9
KL <kl.calamay@blackswan.ventures>
2017-07-19 11:52:39 +0800

    Fixes broken tests

--- test/controllers/event_controller_test.exs
+++ test/controllers/event_controller_test.exs
@@ -2,7 +2,7 @@ defmodule Events.EventControllerTest do
   use Events.ConnCase
 
   alias Events.Event
-  @valid_attrs %{data: "some content", data_details: %{}, date: %{day: 17, hour: 14, min: 0, month: 4, sec: 0, year: 2010}, platform: "visual_search", publisherid: "1", subid: "DEV", type: "some content", url: "some content", uuid: "some content"}
+  @valid_attrs %{data: "some content", data_details: %{}, platform: "visual_search", publisherid: "1", subid: "DEV", type: "some content", url: "some content", uuid: "some content"}
   @invalid_attrs %{uuid: "ffe90ab0-76bf-47e4-84dc-c75aab459b54", subid: "DEV", data_details: %{}}
 
   setup %{conn: conn} do
