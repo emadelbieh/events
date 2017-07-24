0e89d309db154064a14d3ac0d9fe632bc8b916fc
KL <kl.calamay@blackswan.ventures>
2017-07-24 15:28:11 +0800

    Removes Repo/Ecto from Events

--- web/controllers/event_controller.ex
+++ web/controllers/event_controller.ex
@@ -1,7 +1,6 @@
 defmodule Events.EventController do
   use Events.Web, :controller
 
-  alias Events.Event
   alias Events.ElasticSearch
   alias Events.ParamsPreprocessor, as: Preprocessor
 
@@ -12,15 +11,5 @@ defmodule Events.EventController do
     conn
     |> put_status(:created)
     |> json(event_params)
-  end
-
-  def show(conn, %{"id" => id}) do
-    event = Repo.get!(Event, id)
-    render(conn, "show.json", event: event)
-  end
-
-  def search(conn, %{"subids" => subids, "start_date" => start_date, "end_date" => end_date}) do
-    events = Events.UuidPreprocessor.cache(subids, start_date, end_date)
-    json(conn, events)
   end
 end
