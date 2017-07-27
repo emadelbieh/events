28bef03ae287b9db0348ec6c90ab69b8316d7c5a
KL <kl.calamay@blackswan.ventures>
2017-07-27 14:40:33 +0800

    Removes warnings from Elastic search

--- web/services/elastic_search_gateway.ex
+++ web/services/elastic_search_gateway.ex
@@ -1,5 +1,4 @@
 defmodule Events.ElasticSearchGateway do
-  alias HTTPoison, as: HTTP
   alias Events.ElasticSearch, as: ES
 
   @elastic_search_server "https://5e6940f9f653fa78fb2d5813e6ed33db.us-east-1.aws.found.io:9243"
@@ -13,15 +12,15 @@ defmodule Events.ElasticSearchGateway do
     "#{@elastic_search_server}/#{index_pattern}/event/_search"
   end
 
+  def create_index_pattern(nil) do
+    "events-*"
+  end
+
   @doc """
   Creates an index pattern based off a date string in yyyy-mm-dd format
   """
   def create_index_pattern(date) do
     [yyyy, mm, dd] = String.split(date, "-")
     "events-#{yyyy}#{mm}#{dd}"
-  end
-
-  def create_index_pattern(nil) do
-    "events-*"
   end
 end
