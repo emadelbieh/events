e1de78726afbcd69423179b2a86e9bb38439bb78
KL <kl.calamay@blackswan.ventures>
2017-07-27 15:11:31 +0800

    Gets rid of unnecessary modules

--- web/controllers/dau_controller.ex
+++ web/controllers/dau_controller.ex
@@ -1,40 +1,20 @@
 defmodule Events.DauController do
   use Events.Web, :controller
 
-  alias Events.ElasticSearchGateway, as: Gateway
+  alias Events.ElasticSearch, as: ES
 
   def search(conn, %{"subid" => subid, "date" => date, "geo" => geo}) do
-    params = Poison.encode!(search_params(subid, geo))
-    dau = fetch(date, params)
-    json(conn, %{subid: subid, date: date, geo: geo, dau: dau})
+    case ES.search(date, subid, geo) do
+      {:ok, %HTTPoison.Response{body: data}} ->
+        data = Poison.decode!(data)
+        dau = data["aggregations"]["distinct_uuid"]["value"]
+        json(conn, %{subid: subid, date: date, geo: geo, dau: dau})
+      _ ->
+        json(conn, %{error: "error retrieving dau"})
+    end
   end
 
   def search(conn, _params) do
     json(conn, %{error: "wrong parameters"})
-  end
-
-  defp fetch(date, params) do
-    case Gateway.search(date, params) do
-      {:ok, %HTTPoison.Response{body: data}} ->
-        data = Poison.decode!(data)
-        IO.inspect(data)
-        data["aggregations"]["distinct_uuid"]["value"]
-      _ ->
-        :error
-    end
-  end
-
-  defp search_params(subid, geo) do
-    %{
-      size: 0, query: %{ bool: %{ must: [
-            %{ term: %{ subid: subid } },
-            %{ term: %{ geo: geo } }
-      ]}},
-      aggs: %{
-        distinct_uuid: %{
-          cardinality: %{ field: "uuid" }
-        }
-      }
-    }
   end
 end
