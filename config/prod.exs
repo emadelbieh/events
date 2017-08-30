1e842baa939addf6a124b53489a937bdd7a35dfa
KL <kl.calamay@blackswan.ventures>
2017-08-30 11:37:57 +0800

    Run cleaner every 5th of the hour

--- config/prod.exs
+++ config/prod.exs
@@ -24,7 +24,7 @@ config :logger, level: :info
 
 config :quantum, :events,
   cron: [
-    "* * * * *": {"Events.LogCleaner", :delete_log_previous_hour}
+    "5 * * * *": {"Events.LogCleaner", :delete_log_previous_hour}
   ]
 
 # ## SSL Support
